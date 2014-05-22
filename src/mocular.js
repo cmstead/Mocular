c91be2ecba9ce75a2a36de1710f9d91bc9f265b3
Chris Stead <cmstead@gmail.com>
2014-05-21 18:11:24 -0700

    Completed v1 alpha

--- src/mocular.js
+++ src/mocular.js
@@ -10,34 +10,60 @@
 			var expectation = {
 				name: '',
 				args: null,
-				body: function(){},
+				body: null,
 				returnValue: undefined,
 				isMet: false
 			};
 
-			function verifyExpectation(name){
-				var nameIsCorrect = (name === expectation.name);
-				expectation.isMet = (nameIsCorrect) ? true : expectation.isMet;
+            function expectationWasMet(){
+                return expectation.isMet;
+            }
+
+            //Verification methods for function execution
+            function verifyArguments(args){
+                var argumentsOkay = true,
+                    expectedArgs = (expectation.args) ? expectation.args : [];
+
+                for(var i in expectedArgs){
+                    if(expectedArgs.hasOwnProperty(i) && expectedArgs[i] !== args[i]){
+                        argumentsOkay = false;
+                        break;
+                    }
+                }
+
+                return argumentsOkay;
+            }
+
+            function verifyExpectation(name, args){
+				var nameIsCorrect = (name === expectation.name),
+                    argumentsAreCorrect = verifyArguments(args);
+				expectation.isMet = (nameIsCorrect && argumentsAreCorrect);
 			}
 
-			function buildMethodFunction(definition){
-				return function(){
-					var functionBody = (typeof definition.body === 'function') ? definition.body : function(){};
+            //Handlers for the various appended service functions
+            function buildArgumentHandler($service){
+                return function(args){
+                    expectation.args = args;
 
-					verifyExpectation(definition.name);
-					functionBody();
+                    return $service;
+                }
+            }
 
-					return definition.returnValue;
-				}
-			}
+            function buildBodyFunctionHandler($service){
+                return function(bodyFunction){
+                    expectation.body = (typeof bodyFunction === 'function') ? bodyFunction : null;
 
-			function buildExpectation($service){
-				return function(name, args){
+                    return $service;
+                }
+            }
+
+            function buildExpectationHandler($service){
+				return function(name){
 					expectation.name = name;
 
 					//Reset values to default
 					expectation.args = null;
-					expectation.body = function(){};
+					expectation.body = null;
 					expectation.returnValue = undefined;
 					expectation.isMet = false;
 
@@ -45,38 +71,60 @@
 				}
 			}
 
-			function buildArgumentHandler($service){
-				return function(args){
-					expectation.args = args;
+            function buildReturnHandler($service){
+                return function(value){
+                    expectation.returnValue = value;
 
-					return $service;
-				}
-			}
+                    return $service;
+                }
+            }
 
-			function expectationWasMet(){
-				return expectation.isMet;
-			}
+            //Construct replacement function for service method
+            function buildMethodFunction(definition){
+                return function(){
+                    var functionBody = (typeof definition.body === 'function') ? definition.body : function(){},
+                        isExpectedFunction = (definition.name === expectation.name);
 
-			function build(definition){
+                    verifyExpectation(definition.name, arguments);
+
+                    if(isExpectedFunction && typeof expectation.body === 'function'){
+                        expectation.body();
+                    } else {
+                        functionBody();
+                    }
+
+                    return (isExpectedFunction && expectation.returnValue) ?
+                        expectation.returnValue :
+                        definition.returnValue;
+                }
+            }
+
+            //Exposed build function
+            function build(definition){
 				var $service = $injector.get(definition.name),
 					methods = (definition.methods) ? definition.methods : [],
 					methodName;
 
 				for(var i in methods){
-					methodName = methods[i].name;
+                    if(methods.hasOwnProperty(i)){
+                        methodName = methods[i].name;
 
-					$service[methodName] = (typeof $service[methodName] === 'function') ?
-						buildMethodFunction(methods[i]) :
-						$service[methodName];
+                        $service[methodName] = (typeof $service[methodName] === 'function') ?
+                            buildMethodFunction(methods[i]) :
+                            $service[methodName];
+                    }
 				}
 
-				$service.expectCall = buildExpectation($service);
+				$service.expectCall = buildExpectationHandler($service);
 				$service.withArguments = buildArgumentHandler($service);
+                $service.usingFunction = buildBodyFunctionHandler($service);
+                $service.returning = buildReturnHandler($service);
+
 				$service.expectationWasMet = expectationWasMet;
 			}
 
 			return {
-				build: build,
+				build: build
 			};
 		}
 
--- test/mocular.spec.js
+++ test/mocular.spec.js
@@ -63,10 +63,23 @@
 					expect(errorThrown).toBe(false);
 				});
 
+                it('should not rewrite methods which are not specified', function(){
+                    var errorThrown = false;
+
+                    try{
+                        $testService.set();
+                    } catch(error) {
+                        errorThrown = true;
+                    }
+
+                    expect(errorThrown).toBe(true);
+                });
+
 				describe(': rewritten method', function(){
 
 					var testObj = {
-						bodyWasExecuted: false
+						bodyWasExecuted: false,
+                        substituteWasExecuted: false
 					};
 
 					beforeEach(function(){
@@ -103,7 +116,39 @@
 						expect($testService.expectationWasMet()).toBe(true);
 					});
 
-				});
+                    it('should not set expectation.isMet to true if arguments are not correct', function(){
+                        $testService.expectCall('get').withArguments(["test1", "test2"]);
+                        $testService.get('test1');
+
+                        expect($testService.expectationWasMet()).toBe(false);
+                    });
+
+                    it('should set expectation.isMet to true if arguments are correct', function(){
+                        $testService.expectCall('get').withArguments(["test1", "test2"]);
+                        $testService.get('test1', 'test2');
+
+                        expect($testService.expectationWasMet()).toBe(true);
+                    });
+
+                    it('should execute a substitute body function if it is the expected method', function(){
+                        function substitute(){
+                            testObj.substituteWasExecuted = true;
+                        }
+
+                        $testService.expectCall('get').usingFunction(substitute);
+
+                        $testService.get();
+
+                        expect(testObj.substituteWasExecuted).toBe(true);
+                    });
+
+                    it('should return a substitute value if it is the expected method', function(){
+                        $testService.expectCall('get').returning('substitute');
+
+                        expect($testService.get()).toBe('substitute');
+                    });
+
+                });
 
 				describe(': expectCall', function(){
 
@@ -144,6 +189,42 @@
 					});
 
 				});
+
+                describe(': usingFunction', function(){
+                    beforeEach(function(){
+                        $mockingService.build({
+                            name: '$testService'
+                        });
+                    });
+
+                    it('should be a function', function(){
+                        expect(typeof $testService.usingFunction).toBe('function');
+                    });
+
+                    it('should return the service it is attached to', function(){
+                        var $service = $testService.usingFunction();
+
+                        expect($service).toBe($testService);
+                    });
+                });
+
+                describe(': returning', function(){
+                    beforeEach(function(){
+                        $mockingService.build({
+                            name: '$testService'
+                        });
+                    });
+
+                    it('should be a function', function(){
+                        expect(typeof $testService.returning).toBe('function');
+                    });
+
+                    it('should return the service it is attached to', function(){
+                        var $service = $testService.returning();
+
+                        expect($service).toBe($testService);
+                    });
+                });
 
 				describe(': expectationWasMet', function(){
 
